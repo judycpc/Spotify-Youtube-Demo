2a697c889d4e5e5744be43f3f889b6ebc7bf3f28
judycpc <abc0952753111@gmail.com>
2023-05-30 15:57:45 +0800

    comments & cluster

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, json, jsonify
 import random
 from flask_cors import CORS
 
-from db import get_works
-from model import predict_views
+from model import predict_views, predict_comments, clustering
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -20,10 +19,9 @@ def predict():
     prediction['stream'] = random.randint(500, 5000)
     prediction['views'] = predict_views(data)
     prediction['likes'] = random.randint(100, 3000)
-    prediction['comments'] = random.randint(0, 500)
+    prediction['comments'] = predict_comments(data)
 
-    random_idx = [random.randint(0, 20717) for _ in range(3)]
-    similar_work = get_works(random_idx)
+    similar_work = clustering(data)
 
     return jsonify({'prediction': prediction, 'similar_work': similar_work})
 
--- backend/model.py
+++ backend/model.py
@@ -1,13 +1,57 @@
 import joblib
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import warnings
+import random
 
-views_model = joblib.load('Viewsmodel.joblib')
+warnings.filterwarnings("ignore")
+
+views_model = joblib.load('./models/Viewsmodel.joblib')
+comments_model = joblib.load('./models/Commentsmodel.joblib')
+cluster_model = joblib.load('./models/Clustering.joblib')
+
+df = pd.read_csv('./Spotify_Youtube.csv')
+df.dropna(inplace=True)
+df = df[['Danceability', 'Energy', 'Acousticness', 'Valence', 'Tempo', 'Artist', 'Track', 'Uri']]
+df = df.rename(columns={'Artist': 'artist', 'Track': 'track', 'Uri': 'link'})
 
 def predict_views(data):
     X = pd.DataFrame(data, index=[0])
     y = views_model.predict(X)[0]
-
+    
     if y == 0: return "0~100K"
     elif y == 1: return "100K~5M"
     elif y == 2: return "5M~16.25M"
     else: return "Above 16.25M"
+
+def predict_comments(data):
+    X = pd.DataFrame(data, index=[0])
+    y = comments_model.predict(X)[0]
+
+    if y == 0: return "0~50"
+    elif y == 1: return "50~900"
+    elif y == 2: return "900~2K"
+    else: return "Above 2K"
+
+def clustering(data):
+    features = ['Danceability', 'Energy', 'Acousticness', 'Valence', 'Tempo']
+    X = df[features]    # dataset
+    x = pd.DataFrame(data, index=[0], columns=features)    # input
+
+    X = pd.concat([X, x], ignore_index=True).reset_index(drop=True)
+    
+    # Scaling
+    scale_MinMax = MinMaxScaler()
+    X = pd.DataFrame(scale_MinMax.fit_transform(X), columns = X.columns)
+
+    y = list(cluster_model.fit_predict(X))
+    label = y.pop(-1)   # cluster label of input data
+
+    indices = [i for i, v in enumerate(y) if v == label]
+    indices = random.sample(indices, 3)
+    result = df[['artist', 'track', 'link']].iloc[indices].to_dict(orient='records')
+
+    for i, d in enumerate(result):
+        d.update({'key': i+1})
+    
+    return result
